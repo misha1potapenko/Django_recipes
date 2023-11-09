0ecd44b5f13c08ea868d95295bf329936689b655
misha1potapenko <misha1potapenko@yandex.ru>
2023-11-09 13:29:08 +0300

    added new file urls in users

--- recipes/recipes/urls.py
+++ recipes/recipes/urls.py
@@ -25,11 +25,8 @@ from users import views as user_views
 urlpatterns = [
     path("", include("recipesapp.urls")),
     path("admin/", admin.site.urls),
-    path('register/', user_views.register, name="user-register"),
+    path('account/', include('users.urls')),
 
-    # path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="user-login"),
-    # path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="user-logout"),
-    # path('profile/', user_views.profile, name="user-profile"),
 ]
 
 if settings.DEBUG:
--- recipes/users/views.py
+++ recipes/users/views.py
@@ -1,21 +1,33 @@
-# users/views.py
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages
-from . import forms
-
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
 
 
-def register(request):
-  if request.method == "POST":
-    form = forms.UserRegisterForm(request.POST)
-    if form.is_valid():
-      form.save()
-      # cleaned data is a dictionary
-      username = form.cleaned_data.get('username')
-      messages.success(request, f"{username}, you're account is created!")
-      return redirect('recipes-home')
-  else:
-    form = forms.UserRegisterForm()
-  return render(request, 'users/register.html', {'form': form})
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+        return render(request, 'users/login.html', {'form': form})
+
+
+@login_required
+def dashboard(request):
+    return render(request,
+                  'users/dashboard.html',
+                  {'section': 'dashboard'})
